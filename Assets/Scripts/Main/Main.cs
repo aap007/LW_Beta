a0a06a9fcef0c0592bdfe0aaf7baed6761989b67
paul222 <paul@flaxe.eu>
2014-12-10 15:03:21 +0100

    Added Main scene
    for extra build that contains Server and Client

---
+++ Assets/Scripts/Main/Main.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	// Settings
+	public float widthPercent = 0.3f;
+	public float heightPercent = 0.3f;
+
+	// TODO: remove this deprecated GUI, use Canvas object instead
+	void OnGUI() {
+		Rect r = new Rect(Screen.width * (1 - widthPercent) / 2,
+		                  Screen.height * (1 - heightPercent) / 2,
+		                  Screen.width * widthPercent,
+		                  Screen.height * heightPercent);
+		
+		// Draw menu
+		GUILayout.BeginArea(r);
+		GUILayout.BeginVertical("box");
+		
+		if (GUILayout.Button ("Server"))
+		{
+			Application.LoadLevel("Scene_Server");
+		}
+		if (GUILayout.Button ("Client"))
+		{
+			Application.LoadLevel("Scene_Client");
+		}
+		
+		GUILayout.EndVertical();
+		GUILayout.EndArea();
+	}
+}
