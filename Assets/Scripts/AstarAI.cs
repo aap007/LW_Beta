2662d5ce71c03d8bcd3f3d95b6eae57922d12070
paul222 <paul@flaxe.eu>
2014-12-17 16:55:42 +0100

    Enemy rotation
    Enemy rotation works. I have to check on the client if it works there as
    well. Totally rebuild the enemy to get it nicely working

--- Assets/Scripts/AstarAI.cs
+++ Assets/Scripts/AstarAI.cs
@@ -11,12 +11,15 @@ public class AstarAI : MonoBehaviour {
 	
 	private Seeker seeker;
 	private CharacterController controller;
+	public Transform body;
+	public Transform compass;
 	
 	//The calculated path
 	public Path path;
 	
 	//The AI's speed per second
 	public float speed = 100;
+	public float turnSpeed = 6.0f;
 	
 	//The max distance from the AI to a waypoint for it to continue to the next waypoint
 	public float nextWaypointDistance = 1f;
@@ -34,7 +37,7 @@ public class AstarAI : MonoBehaviour {
 	public void SetDestination(Vector3 destination) {
 		seeker = GetComponent<Seeker>();
 		controller = GetComponent<CharacterController>();
-		
+				
 		//Start a new path to the targetPosition, return the result to the OnPathComplete function
 		targetPosition = destination;
 		seeker.StartPath (transform.position, targetPosition, OnPathComplete);
@@ -64,6 +67,13 @@ public class AstarAI : MonoBehaviour {
 		Vector3 dir = (path.vectorPath[currentWaypoint]-transform.position).normalized;
 		dir *= speed * Time.fixedDeltaTime;
 		controller.SimpleMove (dir);
+
+		//Rotate front to the next waypoint
+		Vector3 point = path.vectorPath[currentWaypoint];
+		point.y = compass.position.y;
+		compass.LookAt(point);
+		body.rotation = Quaternion.Lerp(body.rotation, compass.rotation, Time.deltaTime * turnSpeed);
+
 		
 		//Check if we are close enough to the next waypoint
 		//If we are, proceed to follow the next waypoint
@@ -72,7 +82,6 @@ public class AstarAI : MonoBehaviour {
 		distance.y = 0;
 		if (distance.magnitude < nextWaypointDistance) {
 			currentWaypoint++;
-			return;
 		}
 	}
 } 
